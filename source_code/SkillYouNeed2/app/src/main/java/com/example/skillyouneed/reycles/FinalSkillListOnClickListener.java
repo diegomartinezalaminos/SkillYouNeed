1bcc6dbc0a22b015d666ee677e9f6999f84b5f1a
diegomartinezalaminos <diegomartinezalaminos@gmail.com>
2021-05-31 14:15:36 +0200

    Cambio BBDD y modelos

--- source_code/SkillYouNeed2/app/src/main/java/com/example/skillyouneed/reycles/FinalSkillListOnClickListener.java
+++
@@ -1,7 +0,0 @@
-package com.example.skillyouneed.reycles;
-
-import com.example.skillyouneed.models.FinalSkill;
-
-public interface FinalSkillListOnClickListener {
-    void onIntemClick(FinalSkill finalSkill, int position);
-}
--- source_code/SkillYouNeed2/app/src/main/java/com/example/skillyouneed/reycles/GadgetListOnClickListener.java
+++
@@ -1,7 +0,0 @@
-package com.example.skillyouneed.reycles;
-
-import com.example.skillyouneed.models.OptionItem;
-
-public interface GadgetListOnClickListener {
-    void onIntemClick(OptionItem optionItem, int position);
-}
